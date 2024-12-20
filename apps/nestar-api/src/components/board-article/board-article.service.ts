f53b7f4f0c34ca4f4d0ba59251053ec11813d931
ADAM <azizbekrustamovic@gmail.com>
2024-12-20 17:02:03 +0900

    Feat:develop LiketargetBoardArticle Graphql API

--- apps/nestar-api/src/components/board-article/board-article.service.ts
+++ apps/nestar-api/src/components/board-article/board-article.service.ts
@@ -11,6 +11,9 @@ import { ViewGroup } from '../../libs/enums/view.enum';
 import { lookupMember, shapeIntoMongoObjectId } from '../../libs/config';
 import { StatisticModifier, T } from '../../libs/types/common';
 import { Direction, Message } from '../../libs/enums/common.enum';
+import { LikeService } from '../like/like.service';
+import { LikeInput } from '../../libs/dto/like/like.input';
+import { LikeGroup } from '../../libs/enums/like.enum';
 
 @Injectable()
 export class BoardArticleService {
@@ -18,6 +21,8 @@ export class BoardArticleService {
     @InjectModel('BoardArticle') private readonly boardArticleModel: Model<BoardArticle>,
     private readonly memberService: MemberService,
     private readonly viewService: ViewService,
+    private likeService: LikeService, // Service for tracking Likea
+    
   ) {}
 
   public async createBoardArticle(memberId: ObjectId, input: BoardArticleInput): Promise<BoardArticle> {
@@ -121,6 +126,23 @@ export class BoardArticleService {
   
     return result[0];
   }
+
+public async likeTargetBoardArticle(memberId:ObjectId,likeRefId:ObjectId): Promise<BoardArticle>{
+      const target:BoardArticle=await this.boardArticleModel.findOne({_id:likeRefId,articleStatus:BoardArticleStatus.ACTIVE}).exec();
+      if(!target)throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+      const input:LikeInput={
+        memberId:memberId,
+        likeRefId:likeRefId,
+        likeGroup:LikeGroup.ARTICLE,
+      };
+      //LIKE TOGGLE -1
+      const modifier:number= await this.likeService.toggleLike(input);
+      const result=await this.boardArticleStatsEditor({_id:likeRefId,targetKey:"articleLikes",modifier:modifier});
+      if(!result) throw new InternalServerErrorException(Message.SOMETHING_WENT_WRONG);
+      return result;
+  }
+
+
 
    /** ADMIN **/
 
