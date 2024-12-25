f16cb982b1996e4fcd21069391fe9684991e96f5
ADAM <azizbekrustamovic@gmail.com>
2024-12-25 18:57:50 +0900

    Feat: create batchRollBack ,batchproperties,batchAgents logic

--- apps/nestar-batch/src/batch.service.ts
+++ apps/nestar-batch/src/batch.service.ts
@@ -5,4 +5,17 @@ export class BatchService {
   getHello(): string {
     return 'Welcome to Nestar Batch server!';
   }
+
+  public async batchRollBack():Promise <void>{
+    console.log("batchRollBack");
+    
+  }
+
+  public async batchProperties():Promise <void>{
+    console.log("batchProperties");
+  }
+
+  public async batchAgents():Promise <void>{
+    console.log("batchAgents");
+  }
 }
